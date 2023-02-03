3a3e84fa9774436a5c597323f9532bc940f2c420
GrungeElFz <elf0114_@hotmail.com>
2023-02-03 01:04:26 -0600

    Modified:   Added and styled logo img (src/App.svelte)

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
   let name = 'Svelte'
+  let logo = 'https://kit.svelte.dev/_app/immutable/assets/svelte-logo-5c5d7d20.svg'
 </script>
 
+<img alt='logo' src={logo}>
 <h1>Test: {name}</h1>
 
 
 
 <style>
+  img {
+    width: 15rem;
+  }
   h1 {
     color: orangered;
   }
