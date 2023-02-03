29918e61b6a98ae0aa8b793eb44576dc37f66800
GrungeElFz <elf0114_@hotmail.com>
2023-02-03 15:29:58 -0600

    Modified:   Added and styled a click button (src/lib/Counter.svelte) Modified:   Imported and marked up Counter component (src/App.svelte)

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Counter from "./lib/Counter.svelte";
+
   let name = 'Svelte'
   let logo = 'https://kit.svelte.dev/_app/immutable/assets/svelte-logo-5c5d7d20.svg'
   let text = 'This is a <strong>HTML</strong> string test.'
@@ -7,6 +9,8 @@
 <img alt='logo' src={logo}>
 <h1>Test: {name}</h1>
 <p>{@html text}</p>
+
+<Counter />
 
 
 
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<button on:click={increment}>{count} Clicks</button>
+
+
+
+<style>
+  button {
+    padding: 1rem 2rem;
+  }
+</style>
