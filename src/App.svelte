faaf5acfbbb0a4ea44ff7635e419fbdbf0c64796
GrungeElFz <elf0114_@hotmail.com>
2023-02-03 01:17:21 -0600

    Modified:   Added and styled <p> (src/App.svelte)

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   let name = 'Svelte'
   let logo = 'https://kit.svelte.dev/_app/immutable/assets/svelte-logo-5c5d7d20.svg'
+  let text = 'This is a <strong>HTML</strong> string test.'
 </script>
 
 <img alt='logo' src={logo}>
 <h1>Test: {name}</h1>
+<p>{@html text}</p>
 
 
 
@@ -14,5 +16,8 @@
   }
   h1 {
     color: orangered;
+  }
+  p {
+    color: white;
   }
 </style>
